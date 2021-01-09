e3c4438e6ebed1272799e92eeba900586e4ff4cb
Abhishek nayak <sahilservice200@gmail.com>
2021-01-09 18:10:40 +0530

    Modified Now

--- js/script.js
+++ js/script.js
@@ -176,3 +176,37 @@ $(document).ready(function(){
 
 
 
+// RoadMap Carousel
+
+// Owl Script Here 
+$(document).ready(function () {
+    $('#Road .owl-carousel').owlCarousel({
+        items: 1, // This Is The Number of Items You Want To show
+        margin: 20, // This Is The Gap Which You Want To Shows
+        loop: true, // Behaves As A Loop As The Default is False We Need To Change
+        nav: true, // It Will Show THe arrows 
+        autoplay: true, // Playing The Carousel Automatically
+        autoplayTimeout: 4000, // At What Time You Need The Carousel To scroll Automatically
+        dots:false,
+        nav : false,
+
+        // This Is For The Animation Only Occurs For Single Data
+        animateOut: 'slideOutDown',
+        animateIn: 'pulse',
+        rtl : true, // It Will Make The Carousel Slide from Right To left In reverse Order
+
+                
+        responsive: {
+            0: {
+                margin : 0,
+            },
+            600 : {
+                margin : 5,
+            },
+            1000 : {
+                // stagePadding: 50, // It will Show The Cut Out Part From The Both The Sides Of The Carousel
+            },
+        }
+    })
+});
+
